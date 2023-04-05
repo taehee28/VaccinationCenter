64b102a59233b388387c011f6f42ce01ff969b18
Kang Taehee <dev.thk28@gmail.com>
2023-04-05 11:08:16 +0900

    package 구조 변경

--- app/src/main/java/com/thk/vaccinationcenter/ui/map/MainActivity.kt
+++ app/src/main/java/com/thk/vaccinationcenter/ui/map/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.thk.vaccinationcenter
+package com.thk.vaccinationcenter.ui.map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thk.vaccinationcenter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
