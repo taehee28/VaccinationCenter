7dc8f455bbff7a9bcc0c43efb52ad8abaf3e4cbb
taehee28 <dev.thk28@gmail.com>
2023-04-05 22:45:52 +0900

    Hilt 추가 설정 및 라이브러리 추가
    * Application 상속받는 클래스에 @HiltAndroidApp 달아줌
    * manifest에 name 추가
    * MainActivity에 @AndroidEntryPotint 달아줌
    
    * activity-ktx 라이브러리 추가
    * room에서 paging 라이브러리 사용하기 위한 dependency 추가

--- app/src/main/java/com/thk/vaccinationcenter/ui/map/MainActivity.kt
+++ app/src/main/java/com/thk/vaccinationcenter/ui/map/MainActivity.kt
@@ -3,7 +3,9 @@ package com.thk.vaccinationcenter.ui.map
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.thk.vaccinationcenter.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/thk/vaccinationcenter/utils/VaccinationCenterApplication.kt
+++ app/src/main/java/com/thk/vaccinationcenter/utils/VaccinationCenterApplication.kt
@@ -1,4 +1,4 @@
-package com.thk.vaccinationcenter.ui
+package com.thk.vaccinationcenter.utils
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
