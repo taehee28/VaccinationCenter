ce451a184eb3b2935ba21a52279a18bcdfb98607
Kang Taehee <dev.thk28@gmail.com>
2023-04-07 16:02:19 +0900

    지도 사용 기본 설정
    * 네이버 지도 Auth 예외처리 추가
    * 지도 객체 요청 및 콜백 구현
    
    * Manifest에 위치 관련 permission 추가

--- app/src/main/java/com/thk/vaccinationcenter/ui/map/MapActivity.kt
+++ app/src/main/java/com/thk/vaccinationcenter/ui/map/MapActivity.kt
@@ -4,14 +4,44 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.NaverMapSdk.AuthFailedException
+import com.naver.maps.map.OnMapReadyCallback
 import com.thk.vaccinationcenter.R
+import com.thk.vaccinationcenter.databinding.ActivityMapBinding
+import com.thk.vaccinationcenter.utils.logd
+import com.thk.vaccinationcenter.utils.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityMapBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 네이버 지도 예외 처리
+        NaverMapSdk.getInstance(this).onAuthFailedListener =
+            NaverMapSdk.OnAuthFailedListener { onAuthFailed(it) }
+
+        // 지도 객체 요청
+        (binding.map.getFragment<MapFragment>()).getMapAsync(this)
+    }
+
+    private fun onAuthFailed(exception: AuthFailedException) {
+        exception.printStackTrace()
+
+        val message = getString(R.string.toast_map_auth_failed).format(exception.errorCode)
+        showToast(context = this, message = message)
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        map.locationOverlay.isVisible = true
     }
 
     companion object {
--- app/src/main/java/com/thk/vaccinationcenter/utils/Utils.kt
+++ app/src/main/java/com/thk/vaccinationcenter/utils/Utils.kt
@@ -1,5 +1,11 @@
 package com.thk.vaccinationcenter.utils
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.StringRes
 
 inline fun <reified T> T.logd(message: String) = Log.d(T::class.java.simpleName, message)
+
+fun showToast(context: Context, message: String) = Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+fun showToast(context: Context, @StringRes resId: Int) = Toast.makeText(context, resId, Toast.LENGTH_LONG).show()
