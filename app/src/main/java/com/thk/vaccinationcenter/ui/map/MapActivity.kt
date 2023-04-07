ebdc7df0a5d551c5bcba9d0f07f2cf8bdf76dc3d
taehee28 <dev.thk28@gmail.com>
2023-04-07 22:12:32 +0900

    현재 위치 표시 구현
    * FusedLocationSource가 아닌 FusedLocationProviderClient 사용하도록 변경
    * 현재위치 바뀔 때마다 수동으로 현재위치 옮기고 카메라 옮겨주도록 구현

--- app/src/main/java/com/thk/vaccinationcenter/ui/map/MapActivity.kt
+++ app/src/main/java/com/thk/vaccinationcenter/ui/map/MapActivity.kt
@@ -1,16 +1,27 @@
 package com.thk.vaccinationcenter.ui.map
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.NaverMapSdk.AuthFailedException
-import com.naver.maps.map.util.FusedLocationSource
 import com.thk.vaccinationcenter.R
 import com.thk.vaccinationcenter.databinding.ActivityMapBinding
 import com.thk.vaccinationcenter.utils.logd
@@ -18,16 +29,32 @@ import com.thk.vaccinationcenter.utils.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMapBinding
 
     private lateinit var naverMap: NaverMap
-    /*private val locationSource: FusedLocationSource by lazy {
-        FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-    }*/
 
-    private lateinit var locationSource: FusedLocationSource
+    private val isPermissionGranted: Boolean
+        get() = (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) and
+                (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+
+    private val locationClient: FusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private val locationRequest = LocationRequest.Builder(
+        Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+        1000
+    ).build()
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            for (location in locationResult.locations) {
+                onLocationChanged(location)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,12 +65,40 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         NaverMapSdk.getInstance(this).onAuthFailedListener =
             NaverMapSdk.OnAuthFailedListener { onAuthFailed(it) }
 
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
-        // 지도 객체 요청
-        (binding.map.getFragment<MapFragment>()).getMapAsync(this)
+        // 네이버 지도 객체 요청
+        (binding.map.getFragment<MapFragment>()).getMapAsync { map ->
+            naverMap = map.apply {
+                locationOverlay.isVisible = true
+            }
+        }
+
+        // TODO: 퍼미션을 확인하고 각종 초기화 진행하도록 변경
+
+        // 마지막 위치가 있다면 표시
+        locationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location ?: return@addOnSuccessListener
+            onLocationChanged(location)
+        }
 
         checkLocationPermission()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+        if (isPermissionGranted) {
+            locationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        locationClient.removeLocationUpdates(locationCallback)
     }
 
     private fun onAuthFailed(exception: AuthFailedException) {
@@ -53,43 +108,19 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         showToast(context = this, message = message)
     }
 
-    override fun onMapReady(map: NaverMap) {
-        naverMap = map.apply {
-            locationOverlay.isVisible = true
-            this.locationSource = locationSource
-
-            addOnLocationChangeListener { location ->
-                logd(">> location = $location")
-                /*showToast(context = this@MapActivity, message = "${location.latitude}, ${location.longitude}")*/
-            }
-        }
-        naverMap.uiSettings.isLocationButtonEnabled = true
-    }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if (!locationSource.isActivated) {
-                logd(">> 권한 거부됨")
-                naverMap.locationTrackingMode = LocationTrackingMode.None
-            } else {
-                naverMap.locationTrackingMode = LocationTrackingMode.Follow
-            }
-
-            return
-        }
+        logd(">> onRequestPermissionsResult")
+        // TODO: 거부 시 사용 불가하다는 안내 메시지 표시
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun checkLocationPermission() {
-        val isGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PermissionChecker.PERMISSION_GRANTED
 
-        logd(">> isGranted = $isGranted")
-        if (isGranted) return
+        if (isPermissionGranted) return
 
         if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
             // 이전에 거부한 적이 있음
@@ -114,6 +145,22 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             ),
             LOCATION_PERMISSION_REQUEST_CODE
         )
+    }
+
+    /**
+     * 현재 위치가 바뀔 때마다 현재 위치 아이콘의 위치와 카메라 이동을 설정
+     */
+    private fun onLocationChanged(location: Location) {
+        naverMap.also {
+            val coord = LatLng(location)
+
+            it.locationOverlay.apply {
+                position = coord
+                bearing = location.bearing
+            }
+
+            it.moveCamera(CameraUpdate.scrollTo(coord))
+        }
     }
 
     companion object {
