9f8dd538d20d7bf9e334225591b43c25c13a9523
taehee28 <dev.thk28@gmail.com>
2023-04-09 14:31:10 +0900

    DataBinding 적용 시도

--- app/src/main/java/com/thk/vaccinationcenter/ui/map/CenterInfoViewAdapter.kt
+++ app/src/main/java/com/thk/vaccinationcenter/ui/map/CenterInfoViewAdapter.kt
@@ -3,6 +3,7 @@ package com.thk.vaccinationcenter.ui.map
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.naver.maps.map.overlay.InfoWindow
 import com.thk.vaccinationcenter.R
 import com.thk.vaccinationcenter.databinding.ViewCenterInfoBinding
@@ -13,9 +14,20 @@ import com.thk.vaccinationcenter.models.VaccinationCenter
  */
 class CenterInfoViewAdapter(context: Context) : InfoWindow.DefaultViewAdapter(context) {
     override fun getContentView(infoWindow: InfoWindow): View {
-        val binding = ViewCenterInfoBinding.inflate(LayoutInflater.from(context))
+        /*val binding = ViewCenterInfoBinding.inflate(LayoutInflater.from(context))*/
+
+        val binding = DataBindingUtil.inflate<ViewCenterInfoBinding>(
+            LayoutInflater.from(context),
+            R.layout.view_center_info,
+            null,
+            false
+        )
 
         val centerInfo = infoWindow.marker?.tag as VaccinationCenter
+
+        // FIXME: 왜 데이터 바인딩이 안될까...
+        /*binding.centerInfo = centerInfo*/
+
         binding.apply {
             tvCenterName.text = centerInfo.centerName
             tvAddress.text = centerInfo.address
