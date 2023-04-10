3f2ea34408e617996843edd3de1ab26d64377db7
taehee28 <dev.thk28@gmail.com>
2023-04-10 13:13:54 +0900

    SerializedName 추가

--- app/src/main/java/com/thk/vaccinationcenter/models/VaccinationCenter.kt
+++ app/src/main/java/com/thk/vaccinationcenter/models/VaccinationCenter.kt
@@ -3,22 +3,38 @@ package com.thk.vaccinationcenter.models
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 import com.thk.vaccinationcenter.data.utils.DBInfo
 
 @Entity(tableName = DBInfo.TABLE_NAME)
 data class VaccinationCenter(
-    @PrimaryKey val id: Int = 0,
+    @SerializedName("id")
+    @PrimaryKey
+    val id: Int = 0,
+    @SerializedName("centerName")
     val centerName: String = "",
+    @SerializedName("sido")
     val sido: String = "",
+    @SerializedName("sigungu")
     val sigungu: String = "",
+    @SerializedName("facilityName")
     val facilityName: String = "",   // 시설 이름
+    @SerializedName("zipCode")
     val zipCode: String = "",        // 우편번호
+    @SerializedName("address")
     val address: String = "",        // 도로명 주소
+    @SerializedName("lat")
     val lat: String = "",            // 위도
+    @SerializedName("lng")
     val lng: String = "",            // 경도
+    @SerializedName("createdAt")
     val createdAt: String = "",
+    @SerializedName("updatedAt")
     val updatedAt: String = "",
+    @SerializedName("centerType")
     val centerType: String = "",
+    @SerializedName("org")
     val org: String = "",
+    @SerializedName("phoneNumber")
     val phoneNumber: String = ""
 )
--- app/src/main/java/com/thk/vaccinationcenter/models/VaccinationCenterResponse.kt
+++ app/src/main/java/com/thk/vaccinationcenter/models/VaccinationCenterResponse.kt
@@ -1,10 +1,18 @@
 package com.thk.vaccinationcenter.models
 
+import com.google.gson.annotations.SerializedName
+
 data class VaccinationCenterResponse(
+    @SerializedName("page")
     val page: Int = 0,
+    @SerializedName("perPage")
     val perPage: Int = 0,
+    @SerializedName("totalCount")
     val totalCount: Int = 0,
+    @SerializedName("currentCount")
     val currentCount: Int = 0,
+    @SerializedName("matchCount")
     val matchCount: Int = 0,
+    @SerializedName("data")
     val data: List<VaccinationCenter>
 )
